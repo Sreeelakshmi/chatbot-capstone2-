8f73f8b3f6a035942dbd47a83ee7e5b890227757
Sreelakshmi <72209376+Sreeelakshmi@users.noreply.github.com>
2025-01-15 19:11:43 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -18,7 +18,7 @@ if "messages" not in st.session_state.keys():  # Initialize the chat messages hi
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    reader = SimpleDirectoryReader(input_dir="data/Seven_Sisters_Travel_Packages_Updated - Seven_Sisters_Travel_Packages_Updated (1).pdf", recursive=True)
+    reader = SimpleDirectoryReader(input_dir="data", recursive=True)
     docs = reader.load_data()
     Settings.llm = OpenAI(
         model="gpt-3.5-turbo",
